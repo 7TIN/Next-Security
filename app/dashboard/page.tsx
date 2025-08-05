461b1c2139761ecb5d2895751c7abbe9a0a97728
7TIN <prasadgujarathi619@gmail.com>
2025-08-06 01:26:01 +0530

    feat: added the sidebar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,13 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 // import { Button } from "@/components/ui/button";
 import LogoutButton from "./LogoutButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export default async function Dashboard() {
   const supabase = await createClient();
@@ -11,11 +18,28 @@ export default async function Dashboard() {
   }
 
   return (
-    <>
-      <div>Dashboard</div>
-      <p>Hello {data.user.email}</p>
-
-      <LogoutButton />
-    </>
+    <div className="flex justify-center items-center min-h-screen">
+      <Card className="w-[380px] ">
+        <CardHeader>
+          <CardDescription>
+              <div>Dashboard</div>
+          </CardDescription>
+          <div className="flex justify-center">
+                <Avatar>
+                  <AvatarImage
+                    className="rounded-full size-15"
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </div>
+        </CardHeader>
+        <CardContent>
+          <p>Hello {data.user.email}</p>
+          <LogoutButton />
+        </CardContent>
+      </Card>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 
-
 export default async function HomePage() { 
 
   return (
